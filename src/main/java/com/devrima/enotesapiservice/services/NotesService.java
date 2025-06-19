d0a74d9e891d7f2c74d1baf4dfa511f9bdf2ea10
Rima Afrin Dishahara <102999055+rimaafrindishahara10@users.noreply.github.com>
2025-06-18 17:00:12 -1200

    File-Upload implemented
    Notes module save with file.

--- src/main/java/com/devrima/enotesapiservice/services/NotesService.java
+++ src/main/java/com/devrima/enotesapiservice/services/NotesService.java
@@ -2,13 +2,16 @@ package com.devrima.enotesapiservice.services;
 
 import com.devrima.enotesapiservice.dto.NotesDto;
 import com.devrima.enotesapiservice.exception.ResourceNotFoundException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface NotesService {
 
     //Save-method
-    Boolean saveNotes(NotesDto notesDto) throws ResourceNotFoundException;
+    Boolean saveNotes(String notes, MultipartFile file) throws ResourceNotFoundException, IOException;
 
     //Get-All-Notes
     List<NotesDto> getAllNotes();
--- src/main/java/com/devrima/enotesapiservice/services/impl/NotesServiceImpl.java
+++ src/main/java/com/devrima/enotesapiservice/services/impl/NotesServiceImpl.java
@@ -2,15 +2,28 @@ package com.devrima.enotesapiservice.services.impl;
 
 import com.devrima.enotesapiservice.dto.NotesDto;
 import com.devrima.enotesapiservice.exception.ResourceNotFoundException;
+import com.devrima.enotesapiservice.models.FileDetails;
 import com.devrima.enotesapiservice.models.Notes;
 import com.devrima.enotesapiservice.repositories.CategoryRepo;
+import com.devrima.enotesapiservice.repositories.FileDetailsRepo;
 import com.devrima.enotesapiservice.repositories.NotesRepo;
 import com.devrima.enotesapiservice.services.NotesService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.FilenameUtils;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class NotesServiceImpl implements NotesService {
@@ -18,23 +31,92 @@ public class NotesServiceImpl implements NotesService {
     private final NotesRepo notesRepo;
     private final ModelMapper mapper;
     private final CategoryRepo categoryRepo;
-    public NotesServiceImpl(NotesRepo notesRepo, ModelMapper mapper, CategoryRepo categoryRepo) {
+    private final FileDetailsRepo fileDetailsRepo;
+    public NotesServiceImpl(NotesRepo notesRepo, ModelMapper mapper, CategoryRepo categoryRepo, FileDetailsRepo fileDetailsRepo) {
         this.notesRepo = notesRepo;
         this.mapper = mapper;
         this.categoryRepo = categoryRepo;
+        this.fileDetailsRepo = fileDetailsRepo;
     }
 
+    @Value ( "${file.upload.path}" )
+    private String uploadPath;
 
+    //SAVE-NOTES-METHOD
     @Override
-    public Boolean saveNotes(NotesDto notesDto) throws ResourceNotFoundException {
+    public Boolean saveNotes(String notes, MultipartFile file) throws ResourceNotFoundException, IOException {
 
+        ObjectMapper ob = new ObjectMapper ();
+        NotesDto notesDto = ob.readValue ( notes, NotesDto.class );
         checkCategoryIsExist(notesDto.getCategory ());
-        Notes notes = mapper.map ( notesDto, Notes.class );
-        Notes saveNotes = notesRepo.save ( notes );
+        Notes notesMap = mapper.map ( notesDto, Notes.class );
+        FileDetails fileDetails= saveFileDetails(file);
+
+        if(!ObjectUtils.isEmpty ( fileDetails )){
+            notesMap.setFileDetails ( fileDetails );
+        }else {
+            notesMap.setFileDetails ( null );
+        }
+
+        Notes saveNotes = notesRepo.save ( notesMap );
         if(!ObjectUtils.isEmpty ( saveNotes )){
             return true;
         }
         return false;
+    }
+
+    //SAVE -FILE-Details
+    private FileDetails saveFileDetails(MultipartFile file) throws IOException {
+
+        if (!ObjectUtils.isEmpty ( file ) &&!file.isEmpty ()){
+            String originalFileName = file.getOriginalFilename ();
+            String extension = FilenameUtils.getExtension ( originalFileName );
+
+            List<String> extensionAllowed = Arrays.asList ( "pdf", "xlsx", "jpg","png" );
+            if (!extensionAllowed.contains ( extension )){
+                throw new IllegalAccessError ("Could not upload with this extension !!,You can upload this kind of extension(pdf,xlsx,jpg)");
+            }
+
+            String rndFileName = UUID.randomUUID ().toString ();
+            String uploadFileName = rndFileName+"."+extension; //dsgrfefeff.pfd
+
+            File fileIo = new File (uploadPath);
+            if (!fileIo.exists ()){
+                fileIo.mkdirs ();
+            }
+            //path : enotes/serviceiml/notes/dsgrfefeff.pfd
+            String storePath = uploadPath.concat ( uploadFileName );
+            //Upload - file
+            long uploadFile = Files.copy ( file.getInputStream (), Paths.get ( storePath ) );
+            if (uploadFile!=0){
+                FileDetails fileDetails = new FileDetails ();
+                fileDetails.setOriginalFileName ( originalFileName );
+                fileDetails.setDisplayFileName ( getDisplayName(originalFileName) );
+                fileDetails.setUploadFileName ( uploadFileName );
+                fileDetails.setFileSize ( file.getSize () );
+                fileDetails.setFilePath ( storePath );
+                FileDetails saveFileDetails = fileDetailsRepo.save ( fileDetails );
+                return saveFileDetails;
+            }
+
+
+        }
+        return null;
+
+    }
+
+    //Create DISPLAY FILE-NAME
+    private String getDisplayName(String originalFileName) {
+
+        //java_programming.pdf
+        String extension = FilenameUtils.getExtension ( originalFileName );
+        String fileName = FilenameUtils.removeExtension ( originalFileName );
+        if (fileName.length ()>8){
+            fileName = fileName.substring ( 0, 7 );
+        }
+        fileName = fileName+"."+extension;
+
+        return fileName;
     }
 
     private void checkCategoryIsExist(NotesDto.CategoryDto category) throws ResourceNotFoundException {
